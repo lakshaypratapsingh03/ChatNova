40608d199fad05b3e87669957fd3657af0f9692c
Lakshya pratap shekhawat <lakshaypratapsinghshekhawat64@gmail.com>
2026-03-25 18:59:45 +0530

    server.js fix

--- server/server.js
+++ server/server.js
@@ -19,10 +19,7 @@ app.post('/api/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(cors());
 
 app.use((req, res, next) => {
   res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
