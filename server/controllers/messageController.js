bef7d6c34766043e48769032902b797e866071b6
Lakshya pratap shekhawat <lakshaypratapsinghshekhawat64@gmail.com>
2026-03-25 18:20:49 +0530

    messageController.js fix

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,4 +1,3 @@
-
 import Chat from "../models/Chat.js"
 import User from "../models/User.js"
 import openai from "../configs/openai.js"
@@ -28,7 +27,7 @@ export const textMessageController = async (req, res) => {
         // Add user message to DB
         chat.messages.push({ role: "user", content: finalPrompt, timestamp: Date.now() });
 
-        await new Promise(resolve => setTimeout(resolve, 1500));
+
 
         // CALL THE API WITH CORRECT MODEL
         const { choices } = await openai.chat.completions.create({
@@ -77,9 +76,9 @@ export const textMessageController = async (req, res) => {
 
         // Handle the 429 specifically
         if (error.status === 429) {
-            return res.json({ 
-                success: false, 
-                message: "Gemini Free Tier limit reached. Please wait 60 seconds." 
+            return res.json({
+                success: false,
+                message: "Gemini Free Tier limit reached. Please wait 60 seconds."
             });
         }
 
@@ -87,3 +86,71 @@ export const textMessageController = async (req, res) => {
     }
 }
 
+// Image Generation Message Controller
+export const imageMessageController = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        // Check credits
+        if (req.user.credits < 2) {
+            return res.json({ success: false, message: "you don't have enough credits to use this feature" })
+        }
+        const { prompt, chatId, isPublished } = req.body
+        // find chat
+        const chat = await Chat.findOne({ userId, _id: chatId })
+
+        // Push user message
+        chat.messages.push({
+            role: "user",
+            content: prompt,
+            timestamp: Date.now(),
+            isImage: false
+        });
+
+
+        // Encode the prompt
+        const encodePrompt = encodeURIComponent(prompt)
+
+        // Construct Imagekit AI generation URL
+        const generatedImageUrl = `${process.env.IMAGEKIT_URL_ENDPOINT}/ik-genimg-prompt-${encodePrompt}/ChatNova/${Date.now()}.png?tr=w-800,h-800`;
+
+        // Trigger generation by fetching from ImageKit
+        const aiImageResponse = await axios.get(generatedImageUrl, {
+            responseType: "arraybuffer",
+            validateStatus: () => true
+        })
+
+        // If generation failed, stop here
+        if (aiImageResponse.status !== 200) {
+            throw new Error("Image generation failed. Please try again.")
+        }
+
+        // Convert to Base64
+        const base64Image = `data:image/png;base64,${Buffer.from(aiImageResponse.data, "binary").toString('base64')}`;
+
+        // Upload to ImageKit Media Library
+        const uploadResponse = await imageKit.upload({
+            file: base64Image,
+            fileName: `${Date.now()}.png`,
+            folder: "ChatNova"
+        })
+
+        const reply = {
+            role: 'assistant',
+            content: uploadResponse.url,
+            timestamp: Date.now(),
+            isImage: true,
+            isPublished
+        }
+
+        res.json({ success: true, reply })
+
+        chat.messages.push(reply)
+        await chat.save()
+
+        await User.updateOne({ _id: userId }, { $inc: { credits: -2 } })
+
+
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+    }
+}
